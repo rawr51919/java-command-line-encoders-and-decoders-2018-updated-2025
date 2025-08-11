a9621c4512e4a4165018d2c3817881eaabd14ff5
rawr51919 <colton51919@gmail.com>
2025-08-11 19:02:22 -0300

    Use logger where it should be in XYZ's command-line tool

--- XYZ/CRXYZ.java
+++ XYZ/CRXYZ.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
-import com.xyz.XYZMessageDigest;
 import jcifs.util.Hexdump;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import com.xyz.XYZMessageDigest;
 public class CRXYZ {
+	private static final Logger logger = Logger.getLogger(CRXYZ.class.getName());
 	public static void main(String[] args) {
 		Scanner k = new Scanner(System.in);
 		XYZMessageDigest exyz = new XYZMessageDigest();
@@ -9,16 +12,16 @@ public class CRXYZ {
 		String xyzstring = "";
 		String decision = "";
 		do {
-			System.out.print("Hello and welcome to the XYZ Hasher. What string do you want to hash today?");
+			logger.log(Level.INFO, "Hello and welcome to the XYZ Hasher. What string do you want to hash today?");
 			data = k.nextLine();
 			byte[] inputbytes = data.getBytes();
 			exyz.engineUpdate(inputbytes, 0, inputbytes.length);
 			byte[] xyzbytes = exyz.engineDigest();
-			xyzstring = new String(Hexdump.toHexString(xyzbytes, 0, xyzbytes.length * 2));
-			System.out.println("The XYZ hash of the string you entered was: " + new String(xyzstring));
-			System.out.println("Do you want to hash another string? (y/n)");
+			xyzstring = Hexdump.toHexString(xyzbytes, 0, xyzbytes.length * 2);
+			logger.log(Level.INFO, "The XYZ hash of the string you entered was: {0}", xyzstring);
+			logger.log(Level.INFO, "Do you want to hash another string? (y/n)");
 		} while (decision.equalsIgnoreCase("y"));
-		System.out.println("Exiting the XYZ Hasher...");
+		logger.log(Level.INFO, "Exiting the XYZ Hasher...");
 		k.close();
 	}
 }
